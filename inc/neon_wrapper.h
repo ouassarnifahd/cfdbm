2f9516c9f28ce0d6b288bc43f1f258c1057c23bf
OUASSARNI Fahd <ouassarnifahd@hotmail.com>
2018-06-28 01:30:23 +0900

    Wednesday debug

---
+++ inc/neon_wrapper.h
@@ -0,0 +1,6 @@
+#ifndef __HEADER_NEON_WRAPPER__
+#define __HEADER_NEON_WRAPPER__
+
+
+
+#endif /* end of include guard: __HEADER_NEON_WRAPPER__ */
---
+++ src/neon_wrapper.c
@@ -0,0 +1,4 @@
+#include "common.h"
+#include "neon_wrapper.h"
+
+#include <arm_neon.h>
